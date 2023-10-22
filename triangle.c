09912f43cd4ddaa2396993d4488e81e6ff057e5f
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-10-22 11:51:10 +0600

    some file changed and some added

--- triangle.c
+++ triangle.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    
+    float a,b,c;
+    scanf("%f %f %f",&a,&b,&c);
+    if((b+c)>a)
+    {
+        float perimeter=a+b+c;
+        printf("Perimetro = %.1f",perimeter);
+    }else
+    {
+        float trap_area=(1/2)*c*(a+b);
+        printf("Area = %.1f",trap_area);
+    }
 }
