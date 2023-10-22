5b6a27fe9d009db6f93795ce5d5086f3bc39b767
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-10-22 10:42:28 +0600

    added 3 file

---
+++ average3.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    int x,y,z;
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a<b&&a<c)
+    {
+        if(a<c)
+        {
+            x=a;
+            if(b<c)
+            {
+                y=b;
+                z=c;
+            }else
+            {
+                y=c;
+                z=b;
+            }
+        }
+    }else if(b<c)
+    {
+        x=a;
+        if(c<a)
+        {
+            y=b;
+            z=c;
+        }else
+        {
+            y=c;
+            z=b;
+        }
+    }else
+    {
+        x=c;
+        if(b<a)
+        {
+            y=b;
+            z=a;
+        }else
+        {
+            y=a;
+            z=b;
+        }
+    }
+
+    printf("%d\n",x);
+    printf("%d\n",y);
+    printf("%d\n",z);
+    printf("\n");
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%d\n",c);
+
+
+
+    return 0;
+}
