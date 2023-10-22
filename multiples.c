eaee320dcaa84a6e71703c5042e3c77ff2b47f1a
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-10-22 16:11:06 +0600

    modified multifle file and added triangle_type file

--- multiples.c
+++ multiples.c
@@ -4,10 +4,10 @@ int main()
     int num1,num2;
     scanf("%d %d",&num1,&num2);
 
-    if(num2%num1==0)
-        printf("Sao Multiplos");
+    if(num2%num1==0||num1%num2==0)
+        printf("Sao Multiplos\n");
     else
-        printf("Nao sao Multiplos");
+        printf("Nao sao Multiplos\n");
 
     return 0;
     
---
+++ triangle_type.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+int main()
+{
+    double a,b,c;
+    double x,y,z;
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    if(a<b&&a<c)
+    {
+        if(a<c)
+        {
+            x=a;
+            if(b<c)
+            {
+                y=b;
+                z=c;
+            }else
+            {
+                y=c;
+                z=b;
+            }
+        }
+    }else if(b<c&&b<a)
+    {
+        x=b;
+        if(c<a)
+        {
+            y=c;
+            z=a;
+        }else
+        {
+            y=a;
+            z=c;
+        }
+    }else
+    {
+        x=c;
+        if(b<a)
+        {
+            y=b;
+            z=a;
+        }else
+        {
+            y=a;
+            z=b;
+        }
+    
+    }
+    printf("z=%lf y=%lf x=%lf\n",z,y,x);
+
+    if(z>=(y+x))
+        printf("NAO FORMA TRIANGULO\n");
+    else if(z*z==(y*y+x*x))
+        printf("TRIANGULO RETANGULO\n");
+    else if(z*z>(y*y+x*x))
+        printf("TRIANGULO OBTUSANGULO\n");
+    else if(z*z<(y*y+x*x))
+        printf("TRIANGULO ACUTANGULO\n");
+
+    if(z==y&&y==x&&x==z)
+        printf("TRIANGULO EQUILATERO\n");
+    else if(z==y||y==x||x==z)
+        printf("TRIANGULO ISOSCELES\n");
+
+    return 0;
+}
