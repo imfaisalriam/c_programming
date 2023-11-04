4880fd789216ecd499e1912a3c82be5e928b198d
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-11-04 10:10:28 +0600

    added and changes file

--- codeforces/Beautiful_Matrix.c
+++ codeforces/Beautiful_Matrix.c
@@ -1,45 +1,27 @@
-#include<stdio.h>
-int row_swap(int i,int j)
-{
-    if (j<=3)
-            {
-            int temp=matrix[i][j+1];
-            matrix[i][j+1]=matrix[i][j];
-            matrix[i][j]=temp;
-            }
-            
-}
+#include <stdio.h>
 int main()
 {
+    int row_for_one, col_for_one;
+    int middle_row = 2, middle_col = 2;
     int matrix[5][5];
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
         {
-            scanf("%d",&matrix[i][j]);
-        }
-        
-    }
+            scanf("%d", &matrix[i][j]);
 
-    for (int i = 0; i < 5; i++)
-    {
-        
-        for (int j = 0; j < 5; j++)
-        {
-            
+            if (matrix[i][j] == 1)
+            {
+                row_for_one = i;
+                col_for_one = j;
+            }
         }
-        
-        
     }
-    printf("\n\n");
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            printf("%d ",matrix[i][j]);
-        }
-        printf("\n");
-    }
+    int row_diff = middle_row - row_for_one;
+    int col_diff = middle_col - col_for_one;
+    int total_step = row_diff + col_diff;
+
+    printf("%d", total_step);
 
     return 0;
 }
