54943c34f4a19c35d96b38b867c7d2178705c64c
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-11-05 17:49:02 +0600

    file added and changed

--- codeforces/Bear_and_Big_Brother.c
+++ codeforces/Bear_and_Big_Brother.c
@@ -2,13 +2,25 @@
 ============================================================================================================
                                        Author: Faisal Ahmed Riam
                              Dipertment of Computer Science and Engineering,
-                    Mawlana Bhasani Science and Technology University,Tangil,Bangladesh.
+                    Mawlana Bhasani Science and Technology University,Tangail,Bangladesh.
 ============================================================================================================
 */
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-
+    int a, b, count = 0;
+    scanf("%d %d", &a, &b);
+    while (a<=b)
+    {
+        a = a * 3;
+        b = b * 2;
+        if (a <= b)
+        {
+            count++;
+        }
+    }
+    
+    printf("%d ", count + 1);
 
     return 0;
 }
