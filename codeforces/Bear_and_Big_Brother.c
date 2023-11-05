5c347318d2f90ee185e6b81bece7e27d92890368
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-11-05 17:10:12 +0600

    changes

---
+++ codeforces/Bear_and_Big_Brother.c
@@ -0,0 +1,14 @@
+/*
+============================================================================================================
+                                       Author: Faisal Ahmed Riam
+                             Dipertment of Computer Science and Engineering,
+                    Mawlana Bhasani Science and Technology University,Tangil,Bangladesh.
+============================================================================================================
+*/
+#include<stdio.h>
+int main()
+{
+
+
+    return 0;
+}
