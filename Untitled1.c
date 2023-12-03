45287e110107148a3e4d95e3f3a97296f68876aa
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-12-03 21:45:37 +0600

    changed

---
+++ Untitled1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+    int i = 0, x = 7, y = 7, z[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("x=%d\ty=%d\t\n\n", --x, y--);
+    printf("x=%d\ty=%d\n\n\n", x, y);
+    z[x--] = --x;
+    z[--y] = --y;
+    for (i = 0; i < 9; i++)
+    {
+        printf("z[%d]=%d\n", i, z[i]);
+    }
+    return 0;
+}
+}
---
+++ beecrowd/BEE1096.c
@@ -0,0 +1,23 @@
+/*
+============================================================================================================
+                                       Author: Faisal Ahmed Riam
+                             Dipertment of Computer Science and Engineering,
+                    Mawlana Bhasani Science and Technology University,Tangail,Bangladesh.
+============================================================================================================
+*/
+#include<stdio.h>
+int main()
+{
+    for (int i = 1; i <=9; i+=2)
+    {
+        for (int j = 7; j >= 5; j--)
+        {
+            printf("I=%d J=%d\n",i,j);
+        }
+        
+    }
+    
+
+
+    return 0;
+}
